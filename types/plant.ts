8672493ed386411575b281b02ebd413a89cd8c83
Sawaki Shuto <swkshuto0208@icloud.com>
2025-01-18 23:34:54 +0900

    feat: データモデルを作成

---
+++ types/plant.ts
@@ -0,0 +1,19 @@
+export type plant = {
+    id: string;
+    name: string;
+    description: string;
+    care_periods: [carePeriod];
+    growth_conditions: growthConditions;
+    }
+
+interface carePeriod {
+    start_date: string;
+    end_date: string;
+    period_type: string;
+}
+
+interface growthConditions {
+    light: string;
+    soil: string;
+    hardiness_zone: string;
+}
