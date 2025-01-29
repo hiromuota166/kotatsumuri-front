c1623e4fadbb059846301a8b0e24ce43a517374a
Sawaki Shuto <swkshuto0208@icloud.com>
2025-01-29 18:44:03 +0900

    fix: plantにキャストした

--- app/api/searchPlant.ts
+++ app/api/searchPlant.ts
@@ -3,6 +3,5 @@ import { plant } from "../../types/plant";
 
 export async function searchPlant(query: string): Promise<plant> {
     const response = await apiClient.get(`/plants?query=${encodeURIComponent(query)}`);
-    console.log(response.data);
-    return response.data;
+    return response.data["plant"] as plant;
 }
