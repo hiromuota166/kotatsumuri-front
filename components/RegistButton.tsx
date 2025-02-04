0495c8f73bcc59cb71c7f9d7742ff3db28f63cea
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-04 13:32:57 +0900

    fix: 登録済みか否かでCSSを変更

--- components/RegistButton.tsx
+++ components/RegistButton.tsx
@@ -4,12 +4,19 @@ import { TouchableOpacity, Text, StyleSheet } from 'react-native';
 interface RegistButtonProps {
     onPress: () => void;
     title: string;
+    isRegist?: boolean;
 }
 
-const RegistButton: React.FC<RegistButtonProps> = ({ onPress, title }) => {
+const RegistButton: React.FC<RegistButtonProps> = ({ onPress, title, isRegist }) => {
     return (
-        <TouchableOpacity style={styles.button} onPress={onPress}>
-            <Text style={styles.buttonText}>{title}</Text>
+        <TouchableOpacity style={
+            isRegist ? styles.buttonRegisted : styles.button
+        } onPress={onPress}>
+            <Text style={
+                isRegist ? styles.buttonTextRegisted : styles.buttonText
+            }>{
+                isRegist ? '登録済み' : title
+                }</Text>
         </TouchableOpacity>
     );
 };
@@ -27,6 +34,19 @@ const styles = StyleSheet.create({
         fontSize: 20,
         fontWeight: 'bold',
     },
+    buttonRegisted: {
+        backgroundColor: '#D9D9D9',
+        padding: 10,
+        borderRadius: 20,
+        alignItems: 'center',
+        margin: 16,
+    },
+    buttonTextRegisted: {
+        color: 'black',
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    
 });
 
 export default RegistButton;
