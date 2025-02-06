719a831e32f7a2fa58ba8cf74dade9dcf47bbb45
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-06 11:54:03 +0900

    fix: リクエストを修正

--- app/api/plant_regist.ts
+++ app/api/plant_regist.ts
@@ -2,7 +2,6 @@ import apiClient from ".";
 
 
 export async function plant_regist(plant_id: string): Promise<number> {
-    const response = await apiClient.post('users/plants');
-    apiClient.params = { plant_id };
+    const response = await apiClient.post('users/plants', {plant_id});
     return response.status;
 }
