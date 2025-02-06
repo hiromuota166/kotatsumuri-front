49d5cd3de28ffd3ba83a2966782424ebd606f664
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-06 11:54:41 +0900

    fix: タブの情報を追加

--- app/screens/tabScreen.tsx
+++ app/screens/tabScreen.tsx
@@ -2,61 +2,75 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { View, Text, Image } from 'react-native';
-import  Home  from '../screens/home/index';
+import Home from '../screens/home/index';
 import Search from '../screens/search/index';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabScreen() {
-  return (
-      <Tab.Navigator
-      screenOptions={({route}) => ({
-        headerShown: false,
-        tabBarIcon: ({focused}) => {
-            if (route.name === 'Home') {
-                return focused ?
-                    <Image style={{
-                        width: 30,
-                        height: 30,
-                        tintColor: '#68A98A',
-                    }} source={require('../../assets/images/tab-home.png')} />
-                    :
-                    <Image style={{
-                        width: 30,
-                        height: 30,
-                        tintColor: 'gray',
-                    }} source={require('../../assets/images/tab-home.png')} />
-            } else if (route.name === 'Search') {
-                return focused ? 
-                    <Image style={{
-                        width: 30,
-                        height: 30,
-                        tintColor: '#68A98A',
-                    }} source={require('../../assets/images/tab-search.png')} />
-                    :
-                    <Image style={{
-                        width: 30,
-                        height: 30,
-                        tintColor: 'gray',
-                    }} source={require('../../assets/images/tab-search.png')} />
-            }
-        },
-        tabBarStyle: {
-            backgroundColor: '#EDF4F0',
-            height: 100,
-            paddingTop: 15,
-        },
-        tabBarActiveTintColor: '#68A98A',
-        tabBarInactiveTintColor: 'gray',
-        tabBarLabelStyle: {
-            fontSize: 14,
-            fontWeight: 'bold',
-        }
-      })}
-      >
-        <Tab.Screen name="Home" component={Home} options={{ tabBarLabel: 'ホーム' }} />
-        <Tab.Screen name="Search" component={Search} options={{ tabBarLabel: '検索' }} />
-      </Tab.Navigator>
-  );
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                headerShown: false,
+                tabBarIcon: ({ focused }) => {
+                    if (route.name === 'Home') {
+                        return focused ?
+                            <Image style={{
+                                width: 30,
+                                height: 30,
+                                tintColor: '#68A98A',
+                            }} source={require('../../assets/images/tab-home.png')} />
+                            :
+                            <Image style={{
+                                width: 30,
+                                height: 30,
+                                tintColor: 'gray',
+                            }} source={require('../../assets/images/tab-home.png')} />
+                    } else if (route.name === 'Search') {
+                        return focused ?
+                            <Image style={{
+                                width: 30,
+                                height: 30,
+                                tintColor: '#68A98A',
+                            }} source={require('../../assets/images/tab-search.png')} />
+                            :
+                            <Image style={{
+                                width: 30,
+                                height: 30,
+                                tintColor: 'gray',
+                            }} source={require('../../assets/images/tab-search.png')} />
+                    } else if (route.name === 'MyPage') {
+                        return focused ?
+                            <Image style={{
+                                width: 30,
+                                height: 30,
+                                tintColor: '#68A98A',
+                            }} source={require('../../assets/images/tab-mypage.png')} />
+                            :
+                            <Image style={{
+                                width: 30,
+                                height: 30,
+                                tintColor: 'gray',
+                            }} source={require('../../assets/images/tab-search.png')} />
+                    }
+                },
+                tabBarStyle: {
+                    backgroundColor: '#EDF4F0',
+                    height: 100,
+                    paddingTop: 15,
+                },
+                tabBarActiveTintColor: '#68A98A',
+                tabBarInactiveTintColor: 'gray',
+                tabBarLabelStyle: {
+                    fontSize: 14,
+                    fontWeight: 'bold',
+                }
+            })}
+        >
+            <Tab.Screen name="Home" component={Home} options={{ tabBarLabel: 'ホーム' }} />
+            <Tab.Screen name="Search" component={Search} options={{ tabBarLabel: '検索' }} />
+            <Tab.Screen name="MyPage" component={Search} options={{ tabBarLabel: 'マイページ' }} />
+        </Tab.Navigator>
+    );
 }
 
