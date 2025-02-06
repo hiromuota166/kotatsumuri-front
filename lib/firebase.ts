26e9f24c162e96489c327ff9e5e6ec81d6eb22a5
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-07 04:56:05 +0900

    todo: ゴリゴリハードコーディングやばい

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,8 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-
+// import { FIREBASE_API_KEY } from '@env';
+const API_KEY = "AIzaSyB-farmmFzPzbsImqsIfOaoP0XM6doVDoI"; // Firebase の API Key
 const firebaseConfig = {
-  apiKey: 'AIzaSyB-farmmFzPzbsImqsIfOaoP0XM6doVDoI',
+  apiKey: API_KEY,
   authDomain: '',
   projectId: 'kotatsumur',
   storageBucket: '',
