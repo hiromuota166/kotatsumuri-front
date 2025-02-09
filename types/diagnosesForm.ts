9cf2d544fbe600b3621dc9759556db59f4303d3c
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-10 04:12:51 +0900

    診断書の方追加

--- types/diagnosesForm.ts
+++ types/diagnosesForm.ts
@@ -1,43 +1,42 @@
 export type DiagnosesForm = {
     plant_id: string,
     name: string,
-    location: location,
-    sunlight: sunlight,
-    ventilation: ventilation,
-    soil_type: soil_type,
-    temperature: temperature,
-    humidity: string,
-    leaf_color: string,
-    stem_root_condition: string,
-    other_parts_condition: string,
+    location?: location,
+    sunlight?: sunlight,
+    ventilation?: ventilation,
+    soil_type?: soil_type,
+    temperature?: temperature,
+    humidity?: string,
+    leaf_color?: string,
+    stem_root_condition?: string,
     //　1~7みたいな数
-    watering_frequency: string,
+    watering_frequency?: string,
     // 液体肥料、固形肥料、無し
-    fertilizer_type: fartilizer_type,
+    fertilizer_type?: fartilizer_type,
     // 1~7みたいな数
-    fertilizing_frequency: string,
-    pesticide_history: pesticide_history,
-    recent_weather: string
+    fertilizing_frequency?: string,
+    pesticide_history?: pesticide_history,
+    recent_weather?: string
 }
 
 
-enum location {
+export enum location {
     indoor = "室内",
     outdoor = "外"
 }
-enum sunlight {
+export enum sunlight {
     shade = "日陰",
     semi_shade = "半日陰",
     sunny = "日当たり"
 }
 
-enum ventilation {
+export enum ventilation {
     poor = "悪い",
     moderate = "普通",
     good = "良い"
 }
 
-enum soil_type {
+export enum soil_type {
     akadama = "赤玉",
     kanuma = "鹿沼",
     hyuga = "日向",
@@ -46,19 +45,19 @@ enum soil_type {
     mizugoke = "水苔",
 }
 
-enum temperature {
+export enum temperature {
     low = "低い",
     medium = "普通",
     high = "高い"
 }
 
-enum fartilizer_type {
+export enum fartilizer_type {
     liquid = "液体肥料",
     solid = "固形肥料",
     none = "無し"
 }
 
-enum pesticide_history {
+export enum pesticide_history {
     yes = "有",
     no = "無"
 }
