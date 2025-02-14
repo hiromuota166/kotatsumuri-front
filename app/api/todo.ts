db0a298fc19abf015886254b6f18b022ac1be5ce
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-15 02:06:04 +0900

    fix: 型修正

--- app/api/todo.ts
+++ app/api/todo.ts
@@ -5,19 +5,18 @@ import { Diagnosis } from "../../types/diagnoses";
 export async function post_todos(todos: [Todo], plant_id: string): Promise<number> {
     const response = await apiClient.post(`/todos/${plant_id}/plants/`, {
         todos: todos.map(todo => ({
-            taskname: todo.task_name,
-            discription: todo.description,
+            taskname: todo.taskname,
+            discription: todo.discription,
             timing: todo.timing,
             priority: todo.priority,
             status: todo.status,
-            duedate: todo.due_date
+            duedate: todo.duedate
         }))
     });
-    console.log(response.data);
     return response.status;
 }
 
 export async function get_todos(plant_id: string): Promise<[Todo]> {
     const response = await apiClient.get(`/todos/${plant_id}/plants/`);
-    return response.data["todos"] as [Todo];
+    return response.data as [Todo];
 }
