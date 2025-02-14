954874ef5ee03f22c9fd6c60787a32b66f7ad591
Sawaki Shuto <swkshuto0208@icloud.com>
2025-02-15 02:05:56 +0900

    fix: 型を修正

--- app/api/searchPlant.ts
+++ app/api/searchPlant.ts
@@ -3,10 +3,11 @@ import { plant } from "../../types/plant";
 
 export async function searchPlant(query: string): Promise<plant> {
     const response = await apiClient.get(`/plants?query=${encodeURIComponent(query)}`);
+    console.log(response.data["plant"] as plant);
     return response.data["plant"] as plant;
 }
 
-export async function getPlant(plant_id: string): Promise<any> {
+export async function getPlant(plant_id: string): Promise<plant> {
     const response = await apiClient.get(`/users/plants/${plant_id}`);
-    return response.data as plant;
+    return response.data["plant"] as plant;
 }
